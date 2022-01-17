ceed2b3ef61238f986f38ac519497376333c3cb7
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-01-17 15:51:29 -0600

    customized button, made li font bigger

--- script.js
+++ script.js
@@ -2,12 +2,32 @@ const addBtn = document.querySelector("#add");
 const subtractBtn = document.querySelector("#subtract");
 const counterNum = document.querySelector('#counter');
 const resetBtn = document.querySelector('#reset');
+const ulElement = document.querySelector("#list-items")
 
 let counter = 0;
 
 function add() {
     counter++
     counterNum.innerText = counter
+
+        // create an element
+        const li = document.createElement('li')
+        li.innerHTML = '<b>Sentence</b> ' + counter
+        li.style = "font-size:30px;"
+
+
+        // const b = document.createElement('b')
+
+
+        // const textNode = document.createTextNode('Sentence ')
+        // b.appendChild(textNode)
+
+        // const textNode2 = document.createTextNode(counter)
+        // li.appendChild(b)
+        // li.appendChild(textNode2)
+
+        // append that element
+        ulElement.appendChild(li)
 }
 
 addBtn.addEventListener('click', add)
@@ -21,5 +41,6 @@ subtractBtn.addEventListener('click', subtract)
 function reset() {
     counter = 0;
     counterNum.innerText = counter
+    location.reload();
 }
 resetBtn.addEventListener('click', reset)
